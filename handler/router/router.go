7838f1c46e926a197c7b0fb63d8ff50f5fa1d170
mbzos45 <90610406+mbzos45@users.noreply.github.com>
2024-01-19 23:50:25 +0900

    sta05

--- handler/router/router.go
+++ handler/router/router.go
@@ -2,11 +2,15 @@ package router
 
 import (
 	"database/sql"
+	"github.com/TechBowl-japan/go-stations/handler"
 	"net/http"
 )
 
 func NewRouter(todoDB *sql.DB) *http.ServeMux {
 	// register routes
 	mux := http.NewServeMux()
+	// Handler 設定
+	healthzHandler := handler.NewHealthzHandler()
+	mux.HandleFunc("/healthz", healthzHandler.ServeHTTP)
 	return mux
 }
--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"database/sql"
 	"github.com/TechBowl-japan/go-stations/db"
-	"github.com/TechBowl-japan/go-stations/handler"
 	"github.com/TechBowl-japan/go-stations/handler/router"
 	"log"
 	"net/http"
@@ -55,9 +54,6 @@ func realMain() error {
 
 	// NOTE: 新しいエンドポイントの登録はrouter.NewRouterの内部で行うようにする
 	mux := router.NewRouter(todoDB)
-	// Handler 設定
-	healthzHandler := handler.NewHealthzHandler()
-	mux.HandleFunc("/healthz", healthzHandler.ServeHTTP)
 	// TODO: サーバーをlistenする
 	err = http.ListenAndServe(defaultPort, mux)
 	if err != nil {
