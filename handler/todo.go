b1eff90ac8b7377e621028837215a1167370ca1e
mbzos45 <90610406+mbzos45@users.noreply.github.com>
2024-02-04 20:48:52 +0900

    sta16

--- handler/todo.go
+++ handler/todo.go
@@ -3,11 +3,12 @@ package handler
 import (
 	"context"
 	"encoding/json"
-	"log"
-	"net/http"
-
 	"github.com/TechBowl-japan/go-stations/model"
 	"github.com/TechBowl-japan/go-stations/service"
+	"io"
+	"log"
+	"net/http"
+	"strconv"
 )
 
 // A TODOHandler implements handling REST endpoints.
@@ -20,6 +21,12 @@ func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	case http.MethodPost:
 		req := &model.CreateTODORequest{}
 		deq := json.NewDecoder(r.Body)
+		defer func(Body io.ReadCloser) {
+			err := Body.Close()
+			if err != nil {
+				log.Println(err)
+			}
+		}(r.Body)
 		if err := deq.Decode(&req); err != nil {
 			log.Println(err)
 		}
@@ -57,6 +64,37 @@ func (h *TODOHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		w.WriteHeader(http.StatusOK)
+	case http.MethodGet:
+		req := &model.ReadTODORequest{}
+		q := r.URL.Query()
+		if idStr := q.Get("prev_id"); idStr != "" {
+			prevId, err := strconv.ParseInt(idStr, 10, 64)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			req.PrevID = prevId
+		}
+		if sizeStr := q.Get("size"); sizeStr != "" {
+			size, err := strconv.ParseInt(sizeStr, 10, 64)
+			if err != nil {
+				log.Println(err)
+				return
+			}
+			req.Size = size
+		} else {
+			req.Size = 3
+		}
+		res, err := h.Read(r.Context(), req)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		if err := json.NewEncoder(w).Encode(res); err != nil {
+			log.Println(err)
+			return
+		}
+		w.WriteHeader(http.StatusOK)
 	default:
 		return
 	}
@@ -78,8 +116,8 @@ func (h *TODOHandler) Create(ctx context.Context, req *model.CreateTODORequest) 
 
 // Read handles the endpoint that reads the TODO_items.
 func (h *TODOHandler) Read(ctx context.Context, req *model.ReadTODORequest) (*model.ReadTODOResponse, error) {
-	_, _ = h.svc.ReadTODO(ctx, 0, 0)
-	return &model.ReadTODOResponse{}, nil
+	todos, err := h.svc.ReadTODO(ctx, req.PrevID, req.Size)
+	return &model.ReadTODOResponse{TODOs: todos}, err
 }
 
 // Update handles the endpoint that updates the TODO_item.
@@ -90,6 +128,6 @@ func (h *TODOHandler) Update(ctx context.Context, req *model.UpdateTODORequest) 
 
 // Delete handles the endpoint that deletes the TODOs.
 func (h *TODOHandler) Delete(ctx context.Context, req *model.DeleteTODORequest) (*model.DeleteTODOResponse, error) {
-	_ = h.svc.DeleteTODO(ctx, nil)
-	return &model.DeleteTODOResponse{}, nil
+	err := h.svc.DeleteTODO(ctx, req.IDs)
+	return &model.DeleteTODOResponse{}, err
 }
